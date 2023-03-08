b0d58231790ed7417a429bfe507a6bc8ae2771b7
drplxrd <kamangasteven00@gmail.com>
2023-03-08 16:25:59 +0200

    to upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def to_upper(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return (ord(c) - 32)
+    else:
+        return ord(c)
+
+def uppercase(string):
+    string_new = ""
+    for character in string:
+        string_new += "%c" % to_upper(character)
+    print("{:s}".format(string_new))
+    
