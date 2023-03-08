0c90b6bb443c3849f07646fa50ac805282f1bf6d
drplxrd <kamangasteven00@gmail.com>
2023-03-08 15:25:01 +0200

    trash

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ number_to_string = repr(number)
 last_digit_string = number_to_string[-1]
 if int(last_digit_string) > 5:
     print(f"Last digit of {number} is {last_digit_string} and is greater than 5")
-elif int(last_digit_string) <6 and int(last_digit_string) != 0:
+elif int(last_digit_string) < 0 and int(last_digit_string) <6 and int(last_digit_string) != 0:
     print(f"Last digit of {number} is {last_digit_string} and is less than 6 and not 0")
 elif int(last_digit_string) == 0:
     print(f"Last digit of {number} is {last_digit_string} and is 0")
