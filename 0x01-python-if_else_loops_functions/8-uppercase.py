5256f8837a28e933e51b1d0e9a3565047d39abad
drplxrd <kamangasteven00@gmail.com>
2023-03-08 16:29:22 +0200

    more fixes

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 def to_upper(c):
     if ord(c) >= 97 and ord(c) <= 122:
         return (ord(c) - 32)
     else:
         return ord(c)
+
 def uppercase(string):
     string_new = ""
     for character in string:
